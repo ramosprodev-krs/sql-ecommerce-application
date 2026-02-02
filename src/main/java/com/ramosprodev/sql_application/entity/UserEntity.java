ae117893cd2dfcf083504894ede98f23a007809b
kauerds <ramosprodev@gmail.com>
2026-02-02 09:55:21 -0300

    Create UserEntity class with role methods and correct associations to other tables

---
+++ src/main/java/com/ramosprodev/sql_application/entity/UserEntity.java
@@ -0,0 +1,82 @@
+package com.ramosprodev.sql_application.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class UserEntity implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(name = "username")
+    @NotBlank(message = "The provided username can't be blank.")
+    @Min(value = 5, message = "The provided username is too short.")
+    @Min(value = 20, message = "The provided username is too long.")
+    private String username;
+
+    @Column(name = "password")
+    @NotBlank(message = "The provided password can't be blank.")
+    @Min(value = 5, message = "The provided password is too short.")
+    private String password;
+
+    @Column(name = "email")
+    @Email(message = "Please provide a valid E-mail.")
+    private String email;
+
+    @Column(name = "user_balance")
+    private BigDecimal userBalance;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "cart_id")
+    private CartEntity cart;
+
+    private UserRole userRole;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(userRole.getRole()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
