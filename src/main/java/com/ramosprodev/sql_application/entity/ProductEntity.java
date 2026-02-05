3e18acc6b6ba0a12ac08e5fb8ac1513140a7f627
kauerds <ramosprodev@gmail.com>
2026-02-05 08:22:26 -0300

    Add customized Jakarta Validations to the ProductEntity class

--- src/main/java/com/ramosprodev/sql_application/entity/ProductEntity.java
+++ src/main/java/com/ramosprodev/sql_application/entity/ProductEntity.java
@@ -1,6 +1,10 @@
 package com.ramosprodev.sql_application.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,15 +24,22 @@ public class ProductEntity {
     private Long id;
 
     @Column(name = "name")
+    @NotBlank(message = "The provided product name can't be blank.")
+    @Min(value = 2, message = "The provided product name is too short.")
+    @Max(value = 20, message = "The provided product name is too long.")
     private String name;
 
-    @Column(name = "stock_quantity")
-    private Integer stockQuantity;
-
     @Column(name = "description")
+    @NotBlank(message = "The provided description can't be blank.")
+    @Min(value = 2, message = "The provided description is too short.")
+    @Max(value = 20, message = "The provided description is too long.")
     private String description;
 
-    @Column(name = "price")
-    private BigDecimal price;
+    @Column(name = "stock_quantity")
+    @NotNull(message = "Stock quantity cannot be negative.")
+    private Integer stockQuantity;
 
+    @Column(name = "price")
+    @NotNull(message = "Price can't be null.")
+    private BigDecimal price;
 }
