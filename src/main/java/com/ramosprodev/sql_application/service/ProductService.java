50fc922f6adaac46e7f656a251f7450efb92960a
kauerds <ramosprodev@gmail.com>
2026-02-05 12:24:41 -0300

    Create ProductService class
    Create all the CRUD methods

---
+++ src/main/java/com/ramosprodev/sql_application/service/ProductService.java
@@ -0,0 +1,87 @@
+package com.ramosprodev.sql_application.service;
+
+import com.ramosprodev.sql_application.dto.ProductDTO;
+import com.ramosprodev.sql_application.entity.ProductEntity;
+import com.ramosprodev.sql_application.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    /*
+     * The following methods compose the CRUD methods, in this case for the product management.
+     * This class acts the same way as the UserService.
+     */
+
+    // CRUD Methods:
+
+    // 1. Create product
+    public ProductEntity createProduct(ProductDTO productDTO) {
+        if (productDTO == null) {
+            throw new IllegalArgumentException("Product DTO can't be null.");
+        }
+
+        ProductEntity product = new ProductEntity();
+        product.setName(productDTO.getName());
+        product.setDescription(productDTO.getDescription());
+        product.setStockQuantity(productDTO.getStockQuantity());
+        product.setPrice(productDTO.getPrice());
+
+        return productRepository.save(product);
+    }
+
+    // 2. Read all products
+    public List<ProductEntity> readAllProducts() {
+        return productRepository.findAll();
+    }
+
+    // 2.1 Read a a single product
+    public ProductEntity readProductById(Long id) {
+        return productRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Product not found."));
+    }
+
+    // 3. Update a single product
+    public ProductEntity updateProduct(Long id, ProductDTO productDTO) {
+        var selectedUser = productRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Product not found."));
+
+        if (productDTO.getName() != null && !productDTO.getName().isBlank()) {
+            selectedUser.setName(productDTO.getName());
+        }
+
+        if (productDTO.getDescription() != null && !productDTO.getDescription().isBlank()) {
+            selectedUser.setDescription(productDTO.getDescription());
+        }
+
+        if (productDTO.getStockQuantity() != null) {
+            selectedUser.setStockQuantity(productDTO.getStockQuantity());
+        }
+
+        if (productDTO.getPrice() != null) {
+            selectedUser.setPrice(productDTO.getPrice());
+        }
+
+        return productRepository.save(selectedUser);
+    }
+
+    // 4. Delete a single product
+    public void deleteProduct(Long id) {
+        if (!productRepository.existsById(id)) {
+            throw new NoSuchElementException("Product not found.");
+        }
+
+        productRepository.deleteById(id);
+
+    }
+
+
+}
