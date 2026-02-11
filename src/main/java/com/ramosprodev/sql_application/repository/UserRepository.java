a00c81a992d7de0bd0746228c8220ef62ee26833
kauerds <ramosprodev@gmail.com>
2026-02-11 10:20:54 -0300

    Create existsByUsername abd existsByEmail methods for better validation

--- src/main/java/com/ramosprodev/sql_application/repository/UserRepository.java
+++ src/main/java/com/ramosprodev/sql_application/repository/UserRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
 }
