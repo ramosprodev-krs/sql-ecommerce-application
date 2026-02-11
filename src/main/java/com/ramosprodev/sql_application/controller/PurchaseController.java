1cf657e8e739934dee272a514af540afe81a0796
kauerds <ramosprodev@gmail.com>
2026-02-11 10:23:36 -0300

    Create PurchaseController class

---
+++ src/main/java/com/ramosprodev/sql_application/controller/PurchaseController.java
@@ -0,0 +1,9 @@
+package com.ramosprodev.sql_application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class PurchaseController {
+}
