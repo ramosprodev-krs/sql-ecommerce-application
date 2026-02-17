f7c02f4121d1f7147cf86448e0ffe19d72243205
kauerds <ramosprodev@gmail.com>
2026-02-17 11:16:41 -0300

    Create new findByUsername method for JWT

--- src/main/java/com/ramosprodev/sql_application/repository/UserRepository.java
+++ src/main/java/com/ramosprodev/sql_application/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.ramosprodev.sql_application.repository;
 import com.ramosprodev.sql_application.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     boolean existsByUsername(String username);
-
+    Optional<UserEntity> findByUsername(String username);
     boolean existsByEmail(String email);
 }
