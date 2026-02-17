90a70e54f285023a10f4109a34286dfc13f6e301
kauerds <ramosprodev@gmail.com>
2026-02-17 11:16:21 -0300

    Replace Getter method with Lombok and create new role Manager

--- src/main/java/com/ramosprodev/sql_application/entity/UserRole.java
+++ src/main/java/com/ramosprodev/sql_application/entity/UserRole.java
@@ -1,16 +1,16 @@
 package com.ramosprodev.sql_application.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum UserRole {
     ADMIN("ROLE_ADMIN"),
-    USER("ROLE_USER");
+    USER("ROLE_USER"),
+    MANAGER("ROLE_MANAGER");
 
     private String role;
 
     UserRole(String role) {
         this.role = role;
-    }
-
-    public String getRole() {
-        return role;
     }
 }
