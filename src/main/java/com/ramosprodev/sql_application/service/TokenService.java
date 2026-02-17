3b17544ec5b23078166fd345760fecf49fcbcbe3
kauerds <ramosprodev@gmail.com>
2026-02-17 11:20:15 -0300

    Create TokenService for token generation and validation

--- src/main/java/com/ramosprodev/sql_application/service/TokenService.java
+++ src/main/java/com/ramosprodev/sql_application/service/TokenService.java
@@ -1,4 +1,45 @@
 package com.ramosprodev.sql_application.service;
 
+import com.ramosprodev.sql_application.entity.UserEntity;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+
+@Service
 public class TokenService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private Long expirationTime;
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    public String generateToken(UserEntity userEntity) {
+        return Jwts.builder()
+                .setSubject(userEntity.getUsername())
+                .claim("role", userEntity.getUserRole().getRole())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String validateToken(String token) {
+        return Jwts.parser()
+                .verifyWith((SecretKey) getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
 }
