98afcaff92bbf7f07d5924f3973567be5923863c
kauerds <ramosprodev@gmail.com>
2026-02-17 11:20:54 -0300

    Complete SecurityFilter class

--- src/main/java/com/ramosprodev/sql_application/filter/SecurityFilter.java
+++ src/main/java/com/ramosprodev/sql_application/filter/SecurityFilter.java
@@ -1,4 +1,49 @@
 package com.ramosprodev.sql_application.filter;
 
-public class SecurityFilter {
+
+import com.ramosprodev.sql_application.entity.UserEntity;
+import com.ramosprodev.sql_application.repository.UserRepository;
+import com.ramosprodev.sql_application.service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    // Manually injecting dependencies
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+
+    public SecurityFilter(TokenService tokenService, UserRepository userRepository) {
+        this.tokenService = tokenService;
+        this.userRepository = userRepository;
+    }
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String header = request.getHeader("Authorization");
+
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+
+            try {
+                String username = tokenService.validateToken(token);
+                UserEntity user = this.userRepository.findByUsername(username).orElseThrow();
+                UsernamePasswordAuthenticationToken auth =
+                        new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
 }
