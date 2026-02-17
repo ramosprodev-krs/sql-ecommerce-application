2149dd00e6b6bf1484b85cda2ee91337e22c4731
kauerds <ramosprodev@gmail.com>
2026-02-17 11:18:00 -0300

    Correct @Min and @Max validations to @Size validations (meant for Strings) and add UserRole attribute (used by admins)

--- src/main/java/com/ramosprodev/sql_application/dto/UserDTO.java
+++ src/main/java/com/ramosprodev/sql_application/dto/UserDTO.java
@@ -1,10 +1,10 @@
 package com.ramosprodev.sql_application.dto;
 
+import com.ramosprodev.sql_application.entity.UserRole;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,18 +17,18 @@ import lombok.Setter;
 public class UserDTO {
 
     @NotBlank(message = "The provided username can't be blank.")
-    @Min(value = 5, message = "The provided username is too short.")
-    @Min(value = 20, message = "The provided username is too long.")
+    @Size(min = 5, max = 20)
     @Schema(description = "Do not use your real name.", example = "random_user123")
     private String username;
 
     @NotBlank(message = "The provided password can't be blank.")
-    @Min(value = 5, message = "The provided password is too short.")
+    @Size(min = 5)
     @Schema(description = "Ensure only you know your password .", example = "My#supersecure$_password4319.")
     private String password;
 
-    @Column(name = "email")
     @Email(message = "Please provide a valid E-mail.")
     @Schema(description = "Remember to use a valid E-mail.", example = "ramosprodev@gmail.com")
     private String email;
+
+    private UserRole userRole;
 }
