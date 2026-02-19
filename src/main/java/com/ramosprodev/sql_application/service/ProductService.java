2be46d5c66611a8a66d9976d824218ad6dfca66a
kauerds <ramosprodev@gmail.com>
2026-02-19 10:42:07 -0300

    Update creation method by taking the current logged in user

--- src/main/java/com/ramosprodev/sql_application/service/ProductService.java
+++ src/main/java/com/ramosprodev/sql_application/service/ProductService.java
@@ -3,6 +3,8 @@ package com.ramosprodev.sql_application.service;
 import com.ramosprodev.sql_application.dto.ProductDTO;
 import com.ramosprodev.sql_application.entity.ProductEntity;
 import com.ramosprodev.sql_application.repository.ProductRepository;
+import com.ramosprodev.sql_application.repository.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,9 +15,12 @@ import java.util.NoSuchElementException;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final UserRepository userRepository;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository,
+                          UserRepository userRepository) {
         this.productRepository = productRepository;
+        this.userRepository = userRepository;
     }
 
     /*
@@ -32,11 +37,17 @@ public class ProductService {
             throw new IllegalArgumentException("Product DTO can't be null.");
         }
 
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        var currentUser = userRepository.findByUsername(username)
+                .orElseThrow(() -> new NoSuchElementException("User not found."));
+
         ProductEntity product = new ProductEntity();
         product.setName(productDTO.getName());
         product.setDescription(productDTO.getDescription());
         product.setStockQuantity(productDTO.getStockQuantity());
         product.setPrice(productDTO.getPrice());
+        product.setCreator(currentUser);
+        currentUser.getProductsList().add(product);
 
         return productRepository.save(product);
     }
