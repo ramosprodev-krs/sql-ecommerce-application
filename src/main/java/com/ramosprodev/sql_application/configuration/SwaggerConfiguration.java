1d5f46b8541e17f7bd21f8a8a0b528dd1d878a70
kauerds <ramosprodev@gmail.com>
2026-02-19 10:38:23 -0300

    Adapt SwaggerConfiguration class for JWT

--- src/main/java/com/ramosprodev/sql_application/configuration/SwaggerConfiguration.java
+++ src/main/java/com/ramosprodev/sql_application/configuration/SwaggerConfiguration.java
@@ -1,4 +1,28 @@
 package com.ramosprodev.sql_application.configuration;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .info(new Info().title("SQL Application API").version("1.0"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
 }
