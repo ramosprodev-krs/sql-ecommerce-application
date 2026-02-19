1004d3573831208d7fae08b6abf0b7e155df01bb
kauerds <ramosprodev@gmail.com>
2026-02-19 10:41:22 -0300

    Add new readOrders() method Complete Swagger documentation Refactor @PreAuthorize

--- src/main/java/com/ramosprodev/sql_application/controller/PurchaseController.java
+++ src/main/java/com/ramosprodev/sql_application/controller/PurchaseController.java
@@ -1,5 +1,6 @@
 package com.ramosprodev.sql_application.controller;
 
+import com.ramosprodev.sql_application.entity.OrderEntity;
 import com.ramosprodev.sql_application.entity.UserEntity;
 import com.ramosprodev.sql_application.service.PurchaseService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -9,12 +10,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestController
@@ -31,11 +31,13 @@ public class PurchaseController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Balance deposited successfully."),
             @ApiResponse(responseCode = "400", description = "Provided quantity invalid.", content = @Content),
+            @ApiResponse(responseCode = "403", description = "You are not authorized to deposit.", content = @Content),
             @ApiResponse(responseCode = "404", description = "User not found.", content = @Content),
             @ApiResponse(responseCode = "500", description = "An Internal Server Error occurred.", content = @Content)
     })
     @PostMapping("/{userId}/deposit")
-    public ResponseEntity<?> depositBalance(@PathVariable Long userId, BigDecimal balance) {
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<UserEntity> depositBalance(@PathVariable Long userId, BigDecimal balance) {
         try {
             UserEntity selectedUser = purchaseService.depositBalance(userId, balance);
             return ResponseEntity.ok(selectedUser);
@@ -52,17 +54,39 @@ public class PurchaseController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Purchase has been successfully completed."),
             @ApiResponse(responseCode = "400", description = "Provided cart is empty or insufficient balance.", content = @Content),
+            @ApiResponse(responseCode = "403", description = "You are not authorized to complete this purchase.", content = @Content),
             @ApiResponse(responseCode = "404", description = "User not found.", content = @Content),
             @ApiResponse(responseCode = "500", description = "An Internal Server Error occurred.", content = @Content)
     })
 
-    @PostMapping("/{userId}/purchase")
-    public ResponseEntity<?> completePurchase(@PathVariable Long userId) {
+    @PostMapping("/complete")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    public ResponseEntity<OrderEntity> completePurchase() {
         try {
-            UserEntity selectedUser = purchaseService.completePurchase(userId);
-            return ResponseEntity.ok(selectedUser);
+            OrderEntity generatedOrder= purchaseService.completePurchase();
+            return ResponseEntity.ok(generatedOrder);
         } catch (IllegalStateException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } catch (DataAccessException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @Operation(summary = "User orders list", description = "Shows the user orders list")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List successfully returned."),
+            @ApiResponse(responseCode = "403", description = "You are not authorized to complete this request", content = @Content),
+            @ApiResponse(responseCode = "404", description = "User not found.", content = @Content),
+            @ApiResponse(responseCode = "500", description = "An Internal Server Error occurred.", content = @Content)
+    })
+    @GetMapping("/orders")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    public ResponseEntity<List<OrderEntity>> readOrders() {
+        try{
+            var orders = purchaseService.readOrders();
+            return ResponseEntity.ok(orders);
         } catch (NoSuchElementException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         } catch (DataAccessException e) {
