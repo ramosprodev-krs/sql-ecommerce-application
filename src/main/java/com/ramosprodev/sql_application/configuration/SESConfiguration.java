df0e954b90bfaf363547f646d412cf2157de936d
kauerds <ramosprodev@gmail.com>
2026-02-23 16:15:11 -0300

    Adapt standard SESConfiguration class Add access and secret access keys
    Add new notes to the class

---
+++ src/main/java/com/ramosprodev/sql_application/configuration/SESConfiguration.java
@@ -0,0 +1,49 @@
+package com.ramosprodev.sql_application.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ses.SesClient;
+
+@Configuration
+public class SESConfiguration {
+
+    /**
+     * This class is responsible for setting the AWS IAM User, in this context, for Amazon SES.
+     * By combining the access key, secret key, and aws region, the basic credentials are created and later implemented
+     * by the SesClient @Bean.
+     **/
+
+
+    /*
+     *  These three attributes use the values contained in the
+     *  project's properties file.
+     */
+
+    // 1. Gets the access key
+    @Value("${aws.accessKeyId}")
+    private String accessKey;
+
+    // 2. Gets the secret access key
+    @Value("${aws.secretAccessKey}")
+    private String secretKey;
+
+    // 3. Gets the aws region
+    @Value("${aws.region}")
+    private String awsRegion;
+
+
+    // The following bean is responsible for setting the provided credentials and returning the sesClient.
+    @Bean
+    public SesClient sesClient() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+        return SesClient.builder()
+                .region(Region.of(awsRegion))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .build();
+    }
+}
